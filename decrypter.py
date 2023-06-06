e296ba007676ae59e069f0c113227fae7978e891
Jhones <jhonesaly@gmail.com>
2023-06-06 15:47:52 -0300

    refactor: generic output

--- decrypter.py
+++ decrypter.py
@@ -16,7 +16,7 @@ def decrypt(file_name):
     os.remove(file_name)
 
     ## criar o arquivo descriptografado
-    new_file = "content.txt"
+    new_file = file.replace(".crypted", "")
     new_file = open(f'{new_file}', "wb")
     new_file.write(decrypt_data)
     new_file.close()
